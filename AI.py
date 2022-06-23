c729eb06503958ff463ea36905c9d9109dc238d5
JeromyKnight <97769672+JeromyKnight@users.noreply.github.com>
2022-06-22 17:52:33 -0700

    Add files via upload

--- AI.py
+++ AI.py
@@ -132,7 +132,9 @@ class AlienInvasion:
             self._fire_bullet()
 
     def _check_keyup_events(self, event):
-        """Respond to keypresses."""
+        """
+        Respond to keypresses.
+        """
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
@@ -148,10 +150,17 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        
+        self._check_bullet_alien_collisions()
 
+    def _check_bullet_alien_collisions(self):
         # Check for any bullets thayt have hit aliens.
         # If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         """
--- settings.py
+++ settings.py
@@ -10,22 +10,22 @@ class Settings:
         Initialize the game's settings.
         """
         # Screen settings
-        self.screen_width = 1200
+        self.screen_width = 800
         self.screen_height = 800
-        self.bg_color = (35, 35, 200)
+        self.bg_color = (50, 50, 150)
 
         # Ship settings
-        self.ship_speed = 1
+        self.ship_speed = .5
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 2
 
         # Alien settings
-        self.alien_speed = .5
+        self.alien_speed = .3
         self.fleet_drop_speed = 10
         self.fleet_direction = 1
 
